08e39271c5c469a20354ac4a9653f3bbd7224e37
Leonardo Parente <leonardo.parente@encora.com>
2022-08-28 19:13:52 -0400

    Go script ok

--- main.go
+++ main.go
@@ -13,6 +13,17 @@ import (
 	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
+func write(str []string) {
+	f, err := os.OpenFile("last_check.txt", os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	for _, value := range str {
+		fmt.Fprintln(f, value)
+	}
+}
+
 func main() {
 	opts := append(
 		// select all the elements after the third element
@@ -29,7 +40,7 @@ func main() {
 	ctx, cancel := chromedp.NewContext(parentCtx)
 	defer cancel()
 
-	var str [3]string
+	str := make([]string, 3)
 	if err := chromedp.Run(ctx,
 		chromedp.Navigate("https://atendimentoiupp.zendesk.com//hc/pt-br/sections/4402572414100"),
 		chromedp.WaitVisible("#main-content"),
@@ -40,26 +51,65 @@ func main() {
 		panic(err)
 	}
 
-	for _, value := range str {
-		b := regexp.MustCompile("azul|smile|latam").MatchString(strings.ToLower(value))
+	var check []string
+	cmatch := 0
+
+	for i, v := range str {
+		b := regexp.MustCompile("azul|smile|latam").MatchString(strings.ToLower(v))
 		var stat int
 		if b {
 			stat = 1
+			cmatch++
+			str[i] = fmt.Sprint(stat) + ", " + v
+			check = append(check, str[i])
 		} else {
 			stat = 0
+			str[i] = fmt.Sprint(stat) + ", " + v
 		}
-		fmt.Printf("%d, %v\n", stat, value)
 	}
 
-	from := mail.NewEmail("iupp promo notify", os.Getenv("secrets.SENDGRID_SENDER_EMAIL"))
-	subject := "iupp exchange promotion"
-	to := mail.NewEmail(os.Getenv("secrets.SENDGRID_TO_NAME"), os.Getenv("secrets.SENDGRID_TO_EMAIL"))
-	plainTextContent := str[1]
-	htmlContent := str[1]
+	if cmatch == 0 {
+		write(str)
+		fmt.Println("No promotion updates, exit")
+		os.Exit(0)
+	}
+
+	dat, err := os.ReadFile("last_check.txt")
+	if err != nil {
+		panic(err)
+	}
+	sData := strings.Split(string(dat), "\n")
+
+	var content []string
+	for _, v := range check {
+		sAppend := true
+		for _, d := range sData {
+			if v == d {
+				sAppend = false
+				break
+			}
+		}
+		if sAppend {
+			content = append(content, v)
+		}
+	}
+
+	write(str)
+	if len(content) == 0 {
+		fmt.Println("No promotion updates, exit")
+		os.Exit(0)
+	}
+
+	from := mail.NewEmail("iupp promo notify", os.Getenv("SENDGRID_SENDER_EMAIL"))
+	subject := "iupp exchange promotion!"
+	to := mail.NewEmail(os.Getenv("SENDGRID_TO_NAME"), os.Getenv("SENDGRID_TO_EMAIL"))
+	plainTextContent := ""
+	htmlContent := fmt.Sprintln(content)
 	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
-	client := sendgrid.NewSendClient(os.Getenv("secrets.SENDGRID_API_KEY"))
-	_, err := client.Send(message)
+	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
+	_, err = client.Send(message)
 	if err != nil {
 		log.Println(err)
 	}
+	fmt.Println("New promotions: ", content)
 }
